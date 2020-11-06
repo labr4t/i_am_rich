ed44e4e273fdc3dae0579abd15e415d1daf9e145
labr4t <labrat.dev@gmail.com>
2020-11-06 16:29:11 +0300

    add image assets

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,14 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.amberAccent,
+        backgroundColor: Colors.blueGrey,
         appBar: AppBar(
           title: Text('I am rich'),
           backgroundColor: Colors.blueGrey[900],
         ),
         body: Center(
           child: Image(
-            image: NetworkImage(
-                'https://lp-cms-production.imgix.net/image_browser/Dead-Sea-Israel.jpg?auto=format&fit=crop&sharp=10&vib=20&ixlib=react-8.6.4&w=850&q=50&dpr=2'),
+            image: AssetImage('images/diamond.png'),
           ),
         ),
       ),
